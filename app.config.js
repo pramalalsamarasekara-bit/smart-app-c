1aaee9023b3d3b3f347462639c39e75eb5f9c432
pramalk <rathna.prama62gmail.com>
2025-11-25 13:33:32 +0300

    Removed EAS Project ID to force new linking

--- app.config.js
+++ app.config.js
@@ -3,14 +3,10 @@
 module.exports = ({ config }) => {
   return {
     ...config,
-    // Android Configuration
-    android: {
-      package: "com.smartshopping.app"
-    },
-    // Plugins array එක සම්පූර්ණයෙන් ඉවත් කර ඇත
+    // ... අනෙකුත් configuration
     extra: {
       eas: {
-        projectId: "ed9407ec-71fb-4969-98ec-3253b870b945"
+        // projectId: "ed9407ec-71fb-4969-98ec-3253b870b945"  <-- මෙය Delete කරන්න
       }
     }
   };
